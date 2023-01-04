f7a5e8a9b1daf5b4930bb650ca4d24a80ad8cdba
barış tunçdemir <baris.tncdmr@gmail.com>
2023-01-04 12:55:14 +0300

    main | es6 ile beraber gelen require export export default methodları kullanıldı

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import { Person } from "./app1";
-import { Employe } from "./app2";
 
-Person.Test()
-Employe.Test()
+// import { Person } from "./app1";
+// import { Employe } from "./app2";
+
+//  const moduleTest = require("./module")
+
+
+
+ 
+
+// moduleTest.test1()
+// moduleTest.test2()
+
+// moduleTest.test1()
+// console.log(moduleTest.name)
+// console.log(moduleTest.person.mail)
+
+//  es6 
+
+// import {test,Person,employess} from "./module"
+
+// Person.Test()
+// console.log(employess.name)
+// test()
+
+
+//  hepsi için 
+import denemeValues, * as module1 from "./module"
+console.log( module1)
+module1.Person.Test()
+console.log(module1.employess.mail)
+module1.test()
+
+
+console.log(denemeValues)
--- src/module.js
+++ src/module.js
@@ -1,7 +1,60 @@
-module.exports = function(){
-    console.log("Test 1")
+// module.exports = function(){
+//     console.log("Test 1")
+// }
+
+// module.exports = function(){
+//     console.log("test 2")
+// }
+
+// module.exports.test1 = function(){
+//     console.log("Test 1")
+// }
+
+// module.exports.test2 = function(){
+//     console.log("test 2")
+// }
+
+
+// module.exports = {
+//     name:"baris",
+//     test1: function(){
+//         console.log("test1")
+//     },
+//     person: {
+//     name: "sedef",
+//     mail: "sedef@gmail.com"
+//     }
+// }
+
+
+// es6 modulleri kullanma 
+
+
+export const name = "baris"
+export function test(){
+    console.log("es6 syntech")
+}
+export class Person{
+  static Test(){
+    console.log("perosn | test ")
+  }
+
 }
 
-module.exports = function(){
-    console.log("test 2")
+export const employess ={
+    name:"sedef",
+    mail: "sedef@gmail.com"
+
 }
+
+// export default  class Deneme{
+//     static test(){
+//         console.log("deneme | default ")
+//     }
+// }
+
+
+const denemeValues =  "denme degeri "
+
+export default denemeValues;
+
