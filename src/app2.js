4ee82b0c49a87b1593275eb8f6d0c35d437317b9
barış tunçdemir <baris.tncdmr@gmail.com>
2023-01-04 10:59:40 +0300

    master | webpack ve babel kullanarak tüm js dosyalarımızı es5 türünde  birleştiren bir proje yaptık

---
+++ src/app2.js
@@ -0,0 +1,5 @@
+export class Employe{
+    static Test(){
+        console.log('Employe: statik test')
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Person } from "./app1";
+import { Employe } from "./app2";
+
+Person.Test()
+Employe.Test()
